9776371711cf54482dcfd47458e828a6e86c0c38
muratatakuma1183 <muratatakuma1183@se-project.co.jp>
2019-09-11 10:55:34 +0900

    バゼルギウス

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+@Controller
 public class UserController {
 
 	@Autowired
